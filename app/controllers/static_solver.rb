99889f61728eb851ec9cfbcec2f37614069a3773
Shakathesaint <leinad80@gmail.com>
2014-11-18 13:06:58 +0100

    restaurato il metodo is_static? per comprendere l'evento 'onclick' e snellire il costo computazionale

--- app/controllers/static_solver.rb
+++ app/controllers/static_solver.rb
@@ -2,44 +2,47 @@ require 'nokogiri'
 require 'open-uri'
 
 class StaticSolver
-$dir = '/home/leinad/RubymineProjects/ruby_bot/bot_testing/'
+  $dir = '/home/leinad/RubymineProjects/ruby_bot/bot_testing/'
 
-attr_reader :form, :method, :input, :on_submit, :action, :url
+  attr_reader :form, :method, :input, :on_submit, :action, :url, :on_click
 
 
-def initialize (url, campo_dati_xpath)
-  @url = url
-  @doc = Nokogiri::HTML(open(url))
+  def initialize (url, campo_dati_xpath)
+    @url = url
+    @doc = Nokogiri::HTML(open(url))
 
     @form = get_form(campo_dati_xpath)
-  form_xpath = form[0].path
+    form_xpath = form[0].path
 
     @method = @form[0]['method']
+
+    #todo: controllare che l'attributo action non sia una stringa vuota altrimenti non può essere un form statico
     @action = @form[0]['action']
     @on_submit = @form[0]['onsubmit']
+    @on_click = @form[0]['onclick']
 
-  #todo: aggiungere onclick
+    #todo: aggiungere onclick
 
-  # ATTENZIONE: in generale avrò più elementi <input...> nella form e devo analizzarli tutti per trovare il submit
-  # il metodo usato qui sotto consiste nell'aggiungere '/input' all'xpath del form e funziona correttamente;
-  # se usassi invece:
-  # - get_element_by_xpath(campo_dati_xpath)
-  # mi restituirebbe un solo <input...> corrispondente all'xpath del campo dati passato
+    # ATTENZIONE: in generale avrò più elementi <input...> nella form e devo analizzarli tutti per trovare il submit
+    # il metodo usato qui sotto consiste nell'aggiungere '/input' all'xpath del form e funziona correttamente;
+    # se usassi invece:
+    # - get_element_by_xpath(campo_dati_xpath)
+    # mi restituirebbe un solo <input...> corrispondente all'xpath del campo dati passato
 
-  @input = @doc.xpath("#{form_xpath}//*[name() = 'input']")
-  # equivale a:
+    @input = @doc.xpath("#{form_xpath}//*[name() = 'input']")
+    # equivale a:
     # input = @doc.xpath("#{form_xpath}/input")
     # ma così scritto funziona anche con documenti xhtml
 
-  puts "Numero di elementi <input> trovati: #{@input.length}"
-end
+    puts "Numero di elementi <input> trovati: #{@input.length}"
+  end
 
 
 #
-  # prende l'xpath di un elemento e risale al form che lo contiene
-  #
-  # @param [String] xpath
-  # @return [Nokogiri::XML::NodeSet] blocco è l'oggetto che racchiude la form
+# prende l'xpath di un elemento e risale al form che lo contiene
+#
+# @param [String] xpath
+# @return [Nokogiri::XML::NodeSet] blocco è l'oggetto che racchiude la form
   def get_form (xpath)
     # risalgo al form di appartenenza - nota: a priori un xpath non è univoco, per questo
     # in realtà blocco[] è un array di risultati
@@ -53,11 +56,11 @@ end
   end
 
 
-  #
-  # prende l'xpath di un elemento e risale all'elemento corrispondente
-  #
-  # @param [String] xpath
-  # @return [Nokogiri::XML::NodeSet] l'oggetto corrispondente all'xpath
+#
+# prende l'xpath di un elemento e risale all'elemento corrispondente
+#
+# @param [String] xpath
+# @return [Nokogiri::XML::NodeSet] l'oggetto corrispondente all'xpath
   def get_element_by_xpath(xpath)
     # restituisco l'elemento corrispondente - nota: a priori un xpath non è univoco, per questo
     # in realtà blocco[] è un array di risultati
@@ -70,15 +73,13 @@ end
     # ATTENZIONE: il submit potrebbe non essere sul primo sottolivello del form, ma anche in livelli successivi
     # inoltre posso avere anche più elementi input per cui devo controllare in tutti gli input[i]
 
-    @input.each do |elemento|
-      if elemento['type'] == 'submit' || elemento['type'] == 'image'
-        @on_submit.nil? ? @static = true : @static = false
-      else
-        @static = false
+    if (@on_submit.nil? and @on_click.nil?) then
+      @input.each do |elemento|
+        return @static = true if elemento['type'] == 'submit' || elemento['type'] == 'image'
       end
-      # puts elemento.to_s
+    else
+      @static = false
     end
-    return @static
   end
 
   def errore(e)
